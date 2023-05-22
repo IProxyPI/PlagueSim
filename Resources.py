838fa1e2f3ed2c19c2e4f26c1c3d972fd44927cb
Proxy <pitzelgabriel@gmail.com>
2023-05-22 15:00:50 -0700

    Added a new resource object type

---
+++ Resources.py
@@ -0,0 +1,8 @@
+
+# Resources that may be moved around the simulation, with current plans thisll
+# just be food- can have the contaminated keyword to say if itll cause sickness
+# however
+class resource_parent():
+    
+    def __init__(self):
+        contaminated = False
