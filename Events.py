c1744988c04a8362d61f393f75ccd7128365330f
Proxy <pitzelgabriel@gmail.com>
2023-05-22 11:29:13 -0700

    Create Events.py

---
+++ Events.py
@@ -0,0 +1,14 @@
+
+
+
+class event_parent():
+    
+    def __init__(self):
+        pass
+    
+
+class infection_event(event_parent):
+    pass
+    # Fill this with relevant data later, mainly with:
+    # time of infection, infector data, infected data, location, etc.
+    # Will start filling this out once we have structure to suggest what to track
