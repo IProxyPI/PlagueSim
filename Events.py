86b53b2448363c87ae8ae47b6b338682bc4787e7
Gabriel <pitzelgabriel@gmail.com>
2023-05-23 21:57:32 -0700

    Added vis code

--- Events.py
+++ Events.py
@@ -8,8 +8,14 @@ class event_parent():
     def __init__(self):
         
         self.time = 0
-        self.day = 0
         self.subject = None # Agent the event has occured to
+        self.type = "Parent"
+    
+    def get_time(self):
+        return self.time
+    
+    def get_type(self):
+        return self.type
     
 # Records the moment in which an infection occurs
 class infection_event(event_parent):
@@ -17,13 +23,21 @@ class infection_event(event_parent):
     def __init__(self):
         self.infection_type = "" # CONTACT, AIRBORN, FOOD
         self.infector = None
+        self.type = "Infection"
     
 # Records whether starvation or infection killed the subject, 
 # holds infection event if infection
-class death_event(event_parent):
+class infection_death_event(event_parent):
     
     def __init__(self):
         
-        self.starvation = False
-        self.infection = False
         self.relevant_infection_event = None
+        self.type = "Infection death"
+        
+# Records whether starvation or infection killed the subject, 
+# holds infection event if infection
+class starvation_death_event(event_parent):
+    
+    def __init__(self):
+        
+        self.type = "Starvation death"
--- Visuals.py
+++ Visuals.py
@@ -1,6 +1,8 @@
 import Agent
 import Locations
 import Parameters
+import Events
+import numpy as np
 
 #   NOTES:
 #   Locations have a capacity, and list of agents within the location.
@@ -21,8 +23,13 @@ import Parameters
 #   agent.sick(self): Returns if this agent is currently sick
 
 
-def print_data_graphs( _list_of_all_agents ):
-    pass
+def print_data_graphs( _list_of_events, _cur_time ):
+    
+    
+    infected_people = np.full(_cur_time, 0)
+    for event in _list_of_events:
+        if (event.get_type() == "Infection"):
+            infected_people[event.get_time()] += 1
 
 def print_map_data( _list_of_all_locations ):
     pass
