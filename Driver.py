36a5ca7a903dec82666597e994e0f8b84142e47c
Gabriel <pitzelgabriel@gmail.com>
2023-05-26 20:15:35 -0700

    Trying to get grids to work, couldnt, tweaking parameters.

--- Driver.py
+++ Driver.py
@@ -113,10 +113,10 @@ def run_quick_sim_v2( _print_interval = 4 ):
     time = 0
     print_interval = _print_interval
     time_until_next_print = print_interval
+        
+    month_range = 0.25
     
-    month_range = 2
-    
-    for i in range(month_range * 30 * 24):
+    for i in range(int(month_range * 30 * 24)):
         
         place_agents_in_world(time, dm, city.get_locations())
         city.update()
@@ -132,7 +132,7 @@ def run_quick_sim_v2( _print_interval = 4 ):
     # Final Print
     Visuals.print_data_graphs(dm.event_list, time, total_agents, False)
     Visuals.print_data_graphs(dm.event_list, time, total_agents, True)
-run_quick_sim_v2(20000000)
+run_quick_sim_v2(1)
 
 # // Runs all tests
 
